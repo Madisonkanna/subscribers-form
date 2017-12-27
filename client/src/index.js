aa07eb55c81e9bd3d6793b19a261f945f5784c9c
madison kanna <madisonkanna@gmail.com>
2017-12-26 23:37:51 -0800

    Comments to myself

--- client/src/index.js
+++ client/src/index.js
@@ -5,5 +5,12 @@ import App from './App';
 import { BrowserRouter, Route } from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker';
 
+
+// Add my subscribers app main component, with routes that hold pages to my home page and confirmation page.
+// <Route exact path="/" component={Home} />
+// <Route exact path="/" component={ConfirmationPage} />
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+ 
