0fff75e038dbec6550bf4d8722039c8441e77d7a
madison kanna <madisonkanna@gmail.com>
2017-12-27 09:02:04 -0800

    Fix name

--- client/src/Home.js
+++ client/src/Home.js
@@ -53,7 +53,7 @@ class Home extends Component {
             <li key={user.id}>{user.username}</li>
             )}
 
-          <Link to="/confirmation" className="open-confirm"> Add a book </Link>
+          <Link to="/confirmation" className="open-confirm"> Add a subscriber to collection book </Link>
           
       </div>
     );
