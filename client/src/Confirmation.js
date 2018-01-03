b0475c3172a9d30444fd94ee85772fbc05739ba0
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:07:10 -0800

    Refactor code

--- client/src/Confirmation.js
+++ client/src/Confirmation.js
@@ -3,15 +3,23 @@ import React, { Component } from "react";
 
 class Confirmation extends React.Component {
   state = {
-    searchResulsts: []
+    users: []
   }
 
-  //code here
+  componentDidMount() {
+    fetch('/users')
+    .then(res => res.json()) 
+    .then(users => this.setState ( { users }));
+
+  }
+
 
   render() {
     //more here
     return (
-      <div className="app">Thank you for subscribing</div>
+      <div className="app">{this.state.users.map( user => 
+            <li key={user.id}>{user.username}</li>
+            )}</div>
       )
   }
 
