311ff15b87753a03e95a4e2635246c1e2d13ecc7
madison kanna <madisonkanna@gmail.com>
2018-01-03 13:28:12 -0800

    Delete old comments and change style

--- client/src/Home.js
+++ client/src/Home.js
@@ -10,46 +10,19 @@ import { Link } from "react-router-dom";
 class Home extends Component {
   // initialize state to an empty array of subscribers
   
-  state = { users: [] }
-
-  //after component mounts
-
-  onInputChange(event) {
-    console.log(event.target.value);
-
-  }
-
-  //need to include form validation  
-  //componentDidMount() {
-    //fetch('/users')
-    //.then(res => res.json())
-    //.then(users => this.setState( { users }));
-
-  //}
-
+  state = { term: '' }
 
    render() {
-    const Content = 'Subscribe'
 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">{Content}</h1>
-
         </header>
         <h1>Subscribe to our newsletter!</h1>
+        <Form />
       
-        {/* only on dev does localhost work */}
-        <a href="/users">Signup yo</a>
-        <Form/>
-
- 
-
-          {this.state.users.map( user => 
-            <li key={user.id}>{user.username}</li>
-            )}
-
+      
         <Link to="/list" input type="submit">List of subscribers  </Link>
       </div>
     );
