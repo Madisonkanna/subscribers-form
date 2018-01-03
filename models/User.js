5f9cb4090c48adbcf0a8e518c5002901b0f7ff15
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:26:48 -0800

    Add more comments

--- models/User.js
+++ models/User.js
@@ -1,9 +1,13 @@
 const mongoose = require('mongoose');
+//Used ES6 destructuring 
 const { Schema } = mongoose;
 
-
+//described all my subscribers properties(what my subscribers will look like): their name, and their email address. 
 const userSchema = new Schema({
   emailAddress: String,
   subscriberName: String
-//describe all my subscribers properties(what my subscribers will look like): their name, and their email address. 
+
 });
+
+
+//Tell mongoose that my new collection needs to be created 
