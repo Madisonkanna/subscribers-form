782a17b50016c9e0b80793cc111e897777f54e15
madison kanna <madisonkanna@gmail.com>
2018-01-03 08:44:31 -0800

    Add to form component

--- client/src/Form.js
+++ client/src/Form.js
@@ -3,27 +3,40 @@ import { Link } from "react-router-dom";
 
 class Form extends React.Component {
 
+  state = { email: '', name: '' }
+
+//        <input value={this.state.term} onChange={(event) => this.setState({ term: event.taret.value })} />;
 
   //code here
 
   render() {
     //more here
     return (
-      <div>
-      <form>
-          <label>
-          Name:
-            <input onChange={this.onInputChange} name="name" />
-          </label>
-          <label>
-          Email:
-            <input onChange={this.onInputChange} email="email" />
-          </label>
-          <Link to="/confirmation" input type="submit">Subscribe  </Link>
-        </form>
-        <form onSubmit={this.handleFormChange}>Submit</form>
 
-      </div>
+
+      <form className="signup-form">
+       <h2>Sign up</h2>
+       <div>
+         <label htmlFor="email">Email address</label>
+         <input type="email" onChange={(event) => this.setState({ email: event.target.value })}
+
+          className="form-control"
+           name="email" />
+       </div>
+
+
+
+
+
+       <div>
+         <label htmlFor="name">Name</label>
+         <input type="name" onChange={(event) => this.setState({ name: event.target.value })}
+ name="name" />
+       </div>
+       <button type="submit">
+          Sign up
+       </button>
+     </form>
 
       )
   }
