9eb4ff381b25c1568efadebb6aea7a2305acebfb
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:21:48 -0800

    Fix typo

--- models/User.js
+++ models/User.js
@@ -2,7 +2,6 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 
-const userSchema = new Schmea({
+const userSchema = new Schema({
 //describe all my subscribers properties: their name, and their email address. 
-
 });
