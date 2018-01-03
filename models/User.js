1c7fa54468fada145e8c7c96cba9e8dc233f026b
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:20:19 -0800

    Create my subscriber schema

--- models/User.js
+++ models/User.js
@@ -1,2 +1,8 @@
 const mongoose = require('mongoose');
 const { Schema } = mongoose;
+
+
+const userSchema = new Schmea({
+//describe all my subscribers properties: their name, and their email address. 
+
+});
