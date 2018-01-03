3f877fa584a995ab366f338bf5f2aa9ef2ea1c2c
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:23:52 -0800

    Add more to my comments

--- models/User.js
+++ models/User.js
@@ -5,5 +5,5 @@ const { Schema } = mongoose;
 const userSchema = new Schema({
   emailAddress: String,
   subscriberName: String
-//describe all my subscribers properties: their name, and their email address. 
+//describe all my subscribers properties(what my subscribers will look like): their name, and their email address. 
 });
