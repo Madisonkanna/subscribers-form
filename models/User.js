3be7171709044ac5e3facd6ea5a9e5e5b93c4ce1
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:22:48 -0800

    Add properties of my user Schema

--- models/User.js
+++ models/User.js
@@ -3,5 +3,7 @@ const { Schema } = mongoose;
 
 
 const userSchema = new Schema({
+  emailAddress: String,
+  subscriberName: String
 //describe all my subscribers properties: their name, and their email address. 
 });
