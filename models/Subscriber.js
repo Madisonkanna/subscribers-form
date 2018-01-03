d7eb705e77c87c38e567e5b458b0bb6f863a2e83
madison kanna <madisonkanna@gmail.com>
2018-01-02 20:29:58 -0800

    Change model name to subscriber to user

--- models/Subscriber.js
+++ models/Subscriber.js
@@ -3,11 +3,12 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 //described all my subscribers properties(what my subscribers will look like): their name, and their email address. 
-const userSchema = new Schema({
+const subscriberSchema = new Schema({
   emailAddress: String,
   subscriberName: String
 
 });
 
 
-//Tell mongoose that my new collection needs to be created 
+//Create model class and tell mongoose it needs to know that that my new collection should be created 
+mongoose.model('subscribers', userSchema);
