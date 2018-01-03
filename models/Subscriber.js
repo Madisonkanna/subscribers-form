d53b17f163228c48f0af47746b070e3b0971bf28
madison kanna <madisonkanna@gmail.com>
2018-01-02 22:25:14 -0800

    Add subscribed property

--- models/Subscriber.js
+++ models/Subscriber.js
@@ -5,10 +5,11 @@ const { Schema } = mongoose;
 //described all my subscribers properties(what my subscribers will look like): their name, and their email address. 
 const subscriberSchema = new Schema({
   emailAddress: String,
-  subscriberName: String
+  name: String,
+  //this property will let us know if the user has clicked on their confirm email or not
+  subscribed: Boolean
 
 });
-
 
 //Create model class and tell mongoose it needs to know that that my new collection should be created 
 mongoose.model('subscribers', subscriberSchema);
