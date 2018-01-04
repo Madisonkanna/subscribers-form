1d05c743bd44545177d4a84bddb1c7281b67df5f
madison kanna <madisonkanna@gmail.com>
2018-01-04 14:44:32 -0800

    Add post request for my subscribers

--- routes/users.js
+++ routes/users.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
 
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.json([
     {id: 1, username: "Madison"},
     {id: 2, username: "Madison two"}
     ]);
 });
+
+router.post('/', (res, req, next) => {
+  console.log('Creating user');
+  //pull the data from the form, in post in the request
+  // create an object in the database using that data
+  //send a response indiciating if it worked
+
+})
+
+//Post request for my user to post data
 
 module.exports = router;
