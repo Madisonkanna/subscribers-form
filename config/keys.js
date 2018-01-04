f8f7a15859c1a5c9de817f5440f61da8461af1d0
madison kanna <madisonkanna@gmail.com>
2018-01-04 14:49:20 -0800

    Add various packages

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  //need to hide in config
+  mongoURI: 'mongodb://mkanna:Icanhandleit1@ds133657.mlab.com:33657/subscribersapp'
+}
+
---
+++ tests/create_test.js
@@ -0,0 +1,8 @@
+//function declaration//function expression
+
+describe('Creating records', () => {
+  it('saves a user', () => {
+    assert(1 + 1 ==== 2);
+
+  });
+})
