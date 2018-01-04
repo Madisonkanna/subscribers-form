ef80ccb520ba178eb96de3070d3293e3a7c1efb9
madison kanna <madisonkanna@gmail.com>
2018-01-03 20:52:05 -0800

    Small style changes

--- client/src/Home.js
+++ client/src/Home.js
@@ -3,13 +3,12 @@ import logo from './logo.svg';
 import './App.css';
 import { Route } from "react-router-dom"
 import Form from './Form';
-
 import { Link } from "react-router-dom";
 
 
 class Home extends Component {
   // initialize state to an empty array of subscribers
-  
+
   state = { term: '' }
 
    render() {
@@ -19,14 +18,15 @@ class Home extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
         </header>
-        <h1>Subscribe to our newsletter!</h1>
-        <Form />
+        <div className="signup">
+          <h1>Subscribe to our newsletter!</h1>
+          <Form className="signup" />
       
-      
-        <Link to="/list" input type="submit">List of subscribers  </Link>
+          <Link to="/list" input type="submit">List of subscribers  </Link>
+        </div>
       </div>
-    );
-  }
+     );
+    }
 }
 
 export default Home;
