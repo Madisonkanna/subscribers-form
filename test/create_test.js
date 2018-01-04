9a456a5e771c13496b6090177ce51348eb29304c
madison kanna <madisonkanna@gmail.com>
2018-01-04 15:32:55 -0800

    Fix typo

--- test/create_test.js
+++ test/create_test.js
@@ -3,6 +3,6 @@ const assert = require('assert');
 describe('Creating records', () => {
   it('saves a user', () => {
     //test assertion
-    assert(1 + 1 ==== 2);
+    assert(1 + 1 === 2);
   });
 })
