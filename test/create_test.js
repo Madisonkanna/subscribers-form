e555a2820e76bb17436a4fe6e4f593f3498b2b1c
madison kanna <madisonkanna@gmail.com>
2018-01-05 11:26:31 -0800

    Fix typo

--- test/create_test.js
+++ test/create_test.js
@@ -1,8 +1,9 @@
 const assert = require('assert');
 const mongoose = require('mongoose');
+const Subscriber = require('../models/subscriber');
 
 describe('Creating records', () => {
-  it('saves a user', () => {
+  it('saves a subscriber', () => {
     //test assertion
     const joe = new Subscriber({ name: 'Joe', emailAddress: 'joe@joe.com', subscribed: false })
 
