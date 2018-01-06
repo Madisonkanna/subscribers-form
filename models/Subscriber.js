e38034c2a0a47ca584d733f4dcc1c2920c2eeafc
madison kanna <madisonkanna@gmail.com>
2018-01-05 22:44:21 -0800

    Add properties back to schema

--- models/Subscriber.js
+++ models/Subscriber.js
@@ -4,17 +4,15 @@ const { Schema } = mongoose;
 
 //described all my subscribers properties(what my subscribers will look like): their name, and their email address. 
 const subscriberSchema = new Schema({
-  //emailAddress: {
-   // type: String,
-    //required: [true, 'Must enter email address']
-  //},
+  email: {
+    type: String,
+    required: [true, 'Must enter email address']
+  },
   name: String,
   //this property will let us know if the user has clicked on their confirm email or not
-  //subscribed: Boolean
-
+  subscribed: Boolean
+  //add default false to subscribed
 });
-
 //Create model class and tell mongoose it needs to know that that my new collection should be created 
-const Subscriber = mongoose.model('subscribers', subscriberSchema);
 
-module.exports = Subscriber;
+module.exports = mongoose.model('subscribers', subscriberSchema);
