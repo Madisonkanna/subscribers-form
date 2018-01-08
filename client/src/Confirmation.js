3ab1813b19fe9c385d7abc496e40f47354d1fb6b
madison kanna <madisonkanna@gmail.com>
2018-01-07 20:40:13 -0800

    Remove wrong code

--- client/src/Confirmation.js
+++ client/src/Confirmation.js
@@ -2,24 +2,12 @@ import React, { Component } from "react";
 
 
 class Confirmation extends React.Component {
-  state = {
-    users: []
-  }
-
-  componentDidMount() {
-    fetch('/users')
-    .then(res => res.json()) 
-    .then(users => this.setState ( { users }));
-
-  }
 
 
   render() {
     //more here
     return (
-      <div className="app">{this.state.users.map( user => 
-            <li key={user.id}>{user.username}</li>
-            )}</div>
+      <div className="app">You've been confirmed!</div>
       )
   }
 
