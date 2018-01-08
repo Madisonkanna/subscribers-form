a37ce69da668ef4cf083ab5c425858a6a6df25d8
madison kanna <madisonkanna@gmail.com>
2018-01-07 20:40:29 -0800

    Add starter code for subscribers to appear

--- client/src/List.js
+++ client/src/List.js
@@ -2,11 +2,24 @@ import React, { Component } from "react";
 
 class List extends React.Component {
 
+  state = {
+    users: []
+  }
+
+  componentDidMount() {
+    fetch('/users')
+    .then(res => res.json()) 
+    .then(users => this.setState ( { users }));
+
+  }
+
   render() {
     //more here
     return (
       <div>
-        <div className="app">Subscriptions List</div>
+        <div className="app">{this.state.users.map( user => 
+          <li key={user.id}>{user.username}</li>
+            )}</div>
         
       </div>
       )
