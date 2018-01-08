d85d795ecfeafa290f037f9883c4544e2fd2042f
madison kanna <madisonkanna@gmail.com>
2018-01-08 11:07:41 -0800

    Start using dotenv for environment variables

---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  //need to hide in config
+  mailgunApiKey: process.env.MAILGUN_API_KEY,
+  mailgunDomain: process.env.MAILGUN_DOMAIN,
+  fromEmail: process.env.FROM_EMAIL,
+  mongoURI: process.env.MONGODB_URI
+}
