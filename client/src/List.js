3fef0223cdd84091db7b3fe4b48c32a0a1f4d791
madison kanna <madisonkanna@gmail.com>
2018-01-09 17:17:28 -0800

    Make users data into a nice form and add search by email

--- client/src/List.js
+++ client/src/List.js
@@ -5,6 +5,13 @@ class List extends React.Component {
   state = {
     users: []
   }
+  
+  handleSearch(event) {
+    console.log(this);
+    console.log(this.input);
+    const users = this.state.users.filter(user => user.email.includes(this.input.value));
+    this.setState({ users })
+  }
 
   componentDidMount() {
     fetch('/users')
@@ -18,10 +25,29 @@ class List extends React.Component {
     //more here
     return (
       <div>
-        <div className="app">{this.state.users.map( user => 
-          <li key={user.id}>{user.email}{user.name}</li>
+        <input type="text" 
+        onKeyPress={this.handleSearch.bind(this)} 
+        ref={(userInput) => this.input = userInput}
+        placeholder="Search by email" />
 
-            )}</div>
+        <table>
+          <tr>
+            <th>Email</th>
+            <th>Name</th>
+          </tr>
+          <tbody>
+          {this.state.users.map(user => {
+            return (
+              <tr key={user._id}>
+                <td>{user.email}</td>
+                <td>{user.name}</td>
+              </tr>    
+            )
+          })}
+  
+          </tbody>
+        </table>
+
         
       </div>
       )
