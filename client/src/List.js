95c889ef5c70b091296dfdea72735324931b1f14
madison kanna <madisonkanna@gmail.com>
2018-01-10 18:13:33 -0800

    Add search filters to email address, name and subscribed at date as well as add icon to toggle search filters

--- client/src/List.js
+++ client/src/List.js
@@ -4,12 +4,12 @@ class List extends React.Component {
 
   state = {
     users: [],
-    query: ''
+    query: '',
+    reversed: false,
+    clickedColumn: null
   }
   
   handleSearch(event) {
-    console.log(this);
-    console.log(this.input);
     this.setState({ query:this.input.value })
   }
     
@@ -17,6 +17,50 @@ class List extends React.Component {
     let {query, users} = this.state;
     if (!query) return users;
     return users.filter(user => user.email.includes(query));
+  }
+  
+  
+  sortData(key) {
+     //take all of state and put it into the state 
+    //spread operator. spread state into const state
+    const state = {...this.state};
+    //togg
+    state.reversed = !state.reversed
+    //touch button its set to true, touch again it goes to false. 
+    //My old code did this in 6 lines
+    // if (state.reversed == true) {
+    //   state.reversed = false;
+    // } else {
+    //   state.reversed = true
+    // }
+    
+    const users = state.users;
+    users.sort((a, b) => {
+      //user's propertiy = a[key]
+      a[key] = a[key] || '';
+      b[key] = b[key] || '';
+      let comparison = 0;
+      //Sort my users
+      if (a[key].toLowerCase() > b[key].toLowerCase()) {
+        comparison = -1;
+      }
+      if (a[key].toLowerCase() < b[key].toLowerCase()) {
+        comparison = 1;
+      }
+      if (this.state.reversed) {
+        //reverses the sort
+        return comparison * -1;
+      }
+      return comparison;  
+      
+      //A single line:
+      //return this.state.reversed ? (comparison * -1) : comparison
+    })
+    state.clickedColumn = key;
+    state.users = users;
+     console.log(state)
+    this.setState(state);
+    
   }
 
   componentDidMount() {
@@ -38,9 +82,24 @@ class List extends React.Component {
 
         <table className="subscriber-list">
           <tr>
-            <th className="subscriber-list">Email</th>
-            <th className="subscriber-list">Name</th>
-            <th className="subscriber-list">Subscribed at</th>
+            <th onClick={this.sortData.bind(this, 'email')} className="subscriber-list">Email
+            {this.state.clickedColumn == 'email' && 
+              <span //I said that if this.state.reversed is true, I will return the down arrow, which signifies that it's in descending order. If it's not true (if it's in ascending order, then I return the up arrow. I'm using the ternary operator to make it look better)
+                className={this.state.reversed ? "glyphicon glyphicon-arrow-down" : "glyphicon glyphicon-arrow-up"}></span> 
+            }
+            </th>
+            
+            <th onClick={this.sortData.bind(this, 'name')} className="subscriber-list">Name 
+            {this.state.clickedColumn == 'name' && 
+              <span className="glyphicon glyphicon-arrow-down"></span> 
+            }
+            </th>
+            <th onClick={this.sortData.bind(this, 'subscribedAt')} className="subscriber-list">
+            Subscribed at 
+            {this.state.clickedColumn == 'subscribedAt' && 
+              <span className="glyphicon glyphicon-arrow-down"></span> 
+            }
+            </th>
           </tr>
           <tbody>
           {this.filteredUsers().map(user => {
