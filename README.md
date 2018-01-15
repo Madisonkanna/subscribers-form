eb009dcc4e85a5abb5afc8b101ca48476a80d517
madison kanna <madisonkanna@gmail.com>
2018-01-15 14:06:38 -0800

    Add test for testing if a new user/subscriber has persisted to database

--- README.md
+++ README.md
@@ -7,3 +7,5 @@ Instructions to run the app:
 2) Install dependencies with yarn
 
 3) In your CL, cd into the app and then run yarn start and open http://localhost:3001/
+
+To run tests with Mocha, use yarn run test
--- test/create_test.js
+++ test/create_test.js
@@ -3,9 +3,16 @@ const mongoose = require('mongoose');
 const Subscriber = require('../models/Subscriber');
 
 describe('Creating records', () => {
-  it('saves a subscriber', () => {
+  it('saves a subscriber', (done) => {
     //test assertion
-    const joe = new Subscriber({ name: 'Joe', emailAddress: 'joe@joe.com', subscribed: false })
-
+    const joe = new Subscriber({ name: 'Joe', email: 'joe@joe.com', subscribedAt: '10/23/92' })
+    //when joe is saved, a promise comes back to us
+    joe.save()
+      .then(() => {
+        //built in flag: it's false when the new user is succesfully saved in DB. !joe.isNew means Joe is saved in Mongo.
+        assert(!joe.isNew);
+        //Add our done callback to let Mocha know that it is only after our then statement executes and our assertion is made, that Mocha can go on to the next test
+        done();
+      });
   });
 })
